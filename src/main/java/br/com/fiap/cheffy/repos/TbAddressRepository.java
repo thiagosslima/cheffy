c99b30607e79ea36cb01a4a0a36d9f58def097fd
Igor Costa <icgsbr@gmail.com>
2025-12-04 20:09:48 -0300

    refactor: remove unused import

--- src/main/java/br/com/fiap/cheffy/repos/TbAddressRepository.java
+++ src/main/java/br/com/fiap/cheffy/repos/TbAddressRepository.java
@@ -1,7 +1,6 @@
 package br.com.fiap.cheffy.repos;
 
 import br.com.fiap.cheffy.domain.TbAddress;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
