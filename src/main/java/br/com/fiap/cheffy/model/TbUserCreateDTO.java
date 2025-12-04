7516c9229de0ed0658419a0802ec69e475cb67ab
Rodrigo F <rodrigo.fc773@gmail.com>
2025-12-04 20:15:48 -0300

    Adicionando o campo id no DTO de resposta

--- src/main/java/br/com/fiap/cheffy/model/TbUserCreateDTO.java
+++ src/main/java/br/com/fiap/cheffy/model/TbUserCreateDTO.java
@@ -1,28 +1,29 @@
 package br.com.fiap.cheffy.model;
 
 import br.com.fiap.cheffy.domain.ProfileType;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record TbUserCreateDTO(
-        @NotNull
+        @NotBlank
         @Size(max = 255)
         String name,
 
-        @NotNull
+        @NotBlank
         @Size(max = 255)
         @TbUserEmailUnique
         String email,
 
-        @NotNull
+        @NotBlank
         @Size(max = 255)
         String login,
 
-        @NotNull
+        @NotBlank
         @Size(min = 8, max = 50, message = "The password needs to be between 8 and 50 chars")
         String password,
 
-        @NotNull
+        @NotBlank
         ProfileType profileType
 ) {
 }
--- src/main/java/br/com/fiap/cheffy/model/TbUserResponseDTO.java
+++ src/main/java/br/com/fiap/cheffy/model/TbUserResponseDTO.java
@@ -1,30 +1,36 @@
 package br.com.fiap.cheffy.model;
 
 import br.com.fiap.cheffy.domain.ProfileType;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.UUID;
+
 
 @Getter
 @Setter
 public class TbUserResponseDTO {
 
-    @NotNull
+    private UUID id;
+
+    @NotBlank
     @Size(max = 255)
     private String name;
 
-    @NotNull
+    @NotBlank
     @Size(max = 255)
     @TbUserEmailUnique
     private String email;
 
-    @NotNull
+    @NotBlank
     @Size(max = 255)
     private String login;
 
+    @NotBlank
     private ProfileType profileType;
 
 }
