4ddba694125771a002edcbfab85f0480325335d9
Victor Reis <victor.sreis@hotmail.com>
2026-01-03 17:56:43 -0300

    fix: remove validação de campos obrigatórios no update de usuário

--- src/main/java/br/com/fiap/cheffy/model/dtos/UserUpdateDTO.java
+++ src/main/java/br/com/fiap/cheffy/model/dtos/UserUpdateDTO.java
@@ -7,11 +7,9 @@ import jakarta.validation.constraints.Size;
 public record UserUpdateDTO(
 
         @Size(min = 3, max = 80, message = "Invalid name")
-        @NotBlank(message = "Name cannot be empty")
         String name,
 
         @Email(message = "e-mail is not valid")
-        @NotBlank(message = "e-mail cannot be empty")
         String email
 ) {
 }
