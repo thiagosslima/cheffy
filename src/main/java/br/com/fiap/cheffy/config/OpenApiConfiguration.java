5d1fa52d096cf5413d726944089c65b537cec148
thiagosslima <thiagosslima@MacBook-Pro-M4-Thiago-Soares.local>
2026-01-20 20:33:44 -0300

    fix/ remove config openapi

--- src/main/java/br/com/fiap/cheffy/config/OpenApiConfiguration.java
+++ src/main/java/br/com/fiap/cheffy/config/OpenApiConfiguration.java
@@ -6,13 +6,7 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.models.PathItem;
-import org.springdoc.core.customizers.OpenApiCustomizer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
-import java.util.Map;
 
 @Configuration
 @OpenAPIDefinition(
@@ -32,20 +26,4 @@ import java.util.Map;
         in = SecuritySchemeIn.HEADER
 )
 public class OpenApiConfiguration {
-    private static final List<String> EXCLUDED_PATHS = List.of(
-            "/api/v1/addresses"
-    );
-
-
-    @Bean
-    public OpenApiCustomizer openApiCustomizer() {
-        return openApi -> openApi.getPaths()
-                .entrySet()
-                .removeIf(this::shouldExcludePath);
-    }
-
-    private boolean shouldExcludePath(Map.Entry<String, PathItem> entry) {
-        return EXCLUDED_PATHS.stream()
-                .anyMatch(excludedPath -> entry.getKey().startsWith(excludedPath));
-    }
 }
